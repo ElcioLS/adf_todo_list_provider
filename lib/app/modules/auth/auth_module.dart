478bf8973c073ddd31f621fc9a1de54c37ffac6c
Elcio Lopes da Silva <elciols@gmail.com>
2022-07-05 21:47:48 -0300

    Tela de cadastro, registre-se Layout

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -1,6 +1,8 @@
 import 'package:adf_todo_list_provider/app/core/modules/todo_list_module.dart';
 import 'package:adf_todo_list_provider/app/modules/auth/login/Login_controller.dart';
 import 'package:adf_todo_list_provider/app/modules/auth/login/login_page.dart';
+import 'package:adf_todo_list_provider/app/modules/auth/register/register_controller.dart';
+import 'package:adf_todo_list_provider/app/modules/auth/register/register_page.dart';
 import 'package:provider/provider.dart';
 
 class AuthModule extends TodoListModule {
@@ -10,9 +12,13 @@ class AuthModule extends TodoListModule {
             ChangeNotifierProvider(
               create: (_) => LoginController(),
             ),
+            ChangeNotifierProvider(
+              create: (_) => RegisterController(),
+            ),
           ],
           routers: {
             '/login': (_) => const LoginPage(),
+            '/register': (_) => const RegisterPage(),
           },
         );
 }
