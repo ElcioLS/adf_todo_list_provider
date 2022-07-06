b2617be52f8853d0321a311ad6fb5e605bf9d1da
Elcio Lopes da Silva <elciols@gmail.com>
2022-07-06 00:00:50 -0300

    firebase config

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -2,6 +2,7 @@ import 'package:adf_todo_list_provider/app/core/database/sqlite_adm_connection.d
 import 'package:adf_todo_list_provider/app/core/ui/todo_list_ui_config.dart';
 import 'package:adf_todo_list_provider/app/modules/auth/auth_module.dart';
 import 'package:adf_todo_list_provider/app/modules/splash/splash_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatefulWidget {
@@ -17,6 +18,7 @@ class _AppWidgetState extends State<AppWidget> {
   @override
   void initState() {
     super.initState();
+    FirebaseAuth auth = FirebaseAuth.instance;
     WidgetsBinding.instance.addObserver(sqliteAdmConnection);
   }
 
